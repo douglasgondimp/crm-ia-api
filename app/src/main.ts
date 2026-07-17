9e1f2f27f23e52970402a232209d564aa69d7843
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-17 00:21:12 -0300

    feat(front): Adiciona os pacotes do primevue e bootstrap na main.ts

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,12 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)
 
 app.mount('#app')
