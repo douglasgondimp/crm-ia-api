d5b623b295d257d6b1f744fc136ee2a3ccdf83b1
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-16 21:11:07 -0300

    feat(api): Cria rotas para autenticação

---
+++ api/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+    Route::get('/me', [\App\Http\Controllers\AuthController::class, 'me']);
+    Route::put('/me', [\App\Http\Controllers\AuthController::class, 'updateProfile']);
+    Route::put('/me/password', [\App\Http\Controllers\AuthController::class, 'updatePassword']);
+});
