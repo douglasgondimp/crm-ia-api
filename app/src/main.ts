6ecfcb73752029584b22d6c45b84753a6fa4a005
Douglas Gondim <douglasgondimp@gmail.com>
2026-07-18 16:29:16 -0300

    feat(front): Importa pacote, adiciona thema e licença do primevue

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,17 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura';
 
 import App from './App.vue'
 import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue)
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    },
+    license: import.meta.env.VITE_PRIMEVUE_LICENSE_KEY
+})
 
 app.mount('#app')
